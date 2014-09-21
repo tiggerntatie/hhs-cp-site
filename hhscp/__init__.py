839c21cd3cfff7eceb53c6404119b1050c4110e8
Eric Dennison <ericd@netdenizen.com>
2014-09-21 18:31:25 +0000

    Changes for new environment under gunicorn/nginx

--- hhscp/__init__.py
+++ hhscp/__init__.py
@@ -7,15 +7,18 @@ __author__ = 'ericdennison'
 
 import sys
 
+
 #sys.path.append('./static/exemplars')
 sys.path.append('./hhscp/static/exemplars')
 
 from flask import Flask
+from hhscp.reverseproxied import ReverseProxied
 
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 from hhscp.pages import *
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+#if __name__ == '__main__':
+#    app.debug = True
+#    app.run()
--- install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-chmod a+w hhscp/data
-mkdir hhscp/userdata
-mkdir hhscp/data
-chmod a+w hhscp/userdata
