f01ccc93f7a56ef82eb3c5b8c894dbfda4247581
Eric Dennison <ericdennison@ericdennison1415.local>
2014-11-01 00:36:25 -0400

    Last fix removed godlike powers for admin. This fix restores proper operation.

--- hhscp/users.py
+++ hhscp/users.py
@@ -87,7 +87,7 @@ class User(object):
     
     def isadminsession(self):
         # Returns TRUE if the administrator is logged in 
-        return 'admin' in session and session['admin'] and session['passhash'] == self.passhash
+        return 'admin' in session and session['admin']
     
     def hashpass(self, password):
         return hashlib.sha512(bytes(password,'utf-8')).hexdigest()
@@ -98,7 +98,7 @@ class User(object):
         
     def login(self, password):
         session['passhash'] = self.hashpass(password)
-        if self.shortname == ADMIN:
+        if self.shortname == ADMIN and self.passhash == session['passhash']:
             session['admin'] = True
         else:
             session['admin'] = None
