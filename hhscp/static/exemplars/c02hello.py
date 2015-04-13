9c6865943a79f26ec699133bb8f836d61b220f13
Eric Dennison <ericd@netdenizen.com>
2015-04-13 17:54:05 -0400

    Update c02hello.py

--- hhscp/static/exemplars/c02hello.py
+++ hhscp/static/exemplars/c02hello.py
@@ -1,6 +1,7 @@
 __author__ = 'ericdennison'
+import datetime
 
 name = input("Please tell me your name: ")
 age = int(input("Please tell me your age: "))
-pythonage = 2012-1989
+pythonage = datetime.date.today().year-1989
 print("Hello, {0}. Python is {1}  years older than you are!".format(name, pythonage-age))
